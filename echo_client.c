74083cd860692ba3264c27ad609b9e2b0895a6d5
vanishcode <vanishcode@gmail.com>
2017-12-24 09:26:00 +0800

    回声服务器初步完成，下一步解决多次循环发送接收的问题

---
+++ echo_client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 100
+
+int main(){
+
+    //创建套接字
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    //向服务器发起请求
+    struct sockaddr_in sockAddr;
+    memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
+    sockAddr.sin_family = PF_INET;
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sockAddr.sin_port = htons(1234);
+    connect(sock, (struct sockaddr*)&sockAddr, sizeof(sockAddr));
+    //获取用户输入的字符串并发送给服务器
+    char bufSend[BUF_SIZE] = {0};
+    printf("Input a string: ");
+    scanf("%s", bufSend);
+    send(sock, bufSend, strlen(bufSend), 0);
+    //接收服务器传回的数据
+    char bufRecv[BUF_SIZE] = {0};
+    recv(sock, bufRecv, BUF_SIZE, 0);
+
+    //输出接收到的数据
+    printf("Message form server: %s\n", bufRecv);
+
+    //关闭套接字
+    close(sock);
+
+    return 0;
+}
---
+++ echo_server.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define BUF_SIZE 100
+
+int main(){
+    //创建套接字
+    int servSock = socket(AF_INET, SOCK_STREAM, 0);
+
+    //绑定套接字
+    struct sockaddr_in sockAddr;
+    memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
+    sockAddr.sin_family = PF_INET;  //使用IPv4地址
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
+    sockAddr.sin_port = htons(1234);  //端口
+    bind(servSock, (struct sockaddr*)&sockAddr, sizeof(sockAddr));
+
+    //进入监听状态
+    listen(servSock, 20);
+
+    //接收客户端请求
+    struct sockaddr_in clntAddr;
+    socklen_t nSize = sizeof(clntAddr);
+    int clntSock = accept(servSock, (struct sockaddr*)&clntAddr, &nSize);
+    char buffer[BUF_SIZE];  //缓冲区
+    int strLen = recv(clntSock, buffer, BUF_SIZE, 0);  //接收客户端发来的数据
+    send(clntSock, buffer, strLen, 0);  //将数据原样返回
+
+    //关闭套接字
+    close(clntSock);
+    close(servSock);
+
+    return 0;
+}
