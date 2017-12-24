3afa7daae77343c532147de3d39d81c764e94ac6
vanishcode <vanishcode@gmail.com>
2017-12-24 10:33:54 +0800

    回声服务器完成

--- echo_client.c
+++ echo_client.c
@@ -15,32 +15,41 @@
 
 #define BUF_SIZE 100
 
-int main(){
+int main()
+{
 
     //创建套接字
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
 
     //向服务器发起请求
     struct sockaddr_in sockAddr;
-    memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
+    memset(&sockAddr, 0, sizeof(sockAddr)); //每个字节都用0填充
     sockAddr.sin_family = PF_INET;
     sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     sockAddr.sin_port = htons(1234);
-    connect(sock, (struct sockaddr*)&sockAddr, sizeof(sockAddr));
-    //获取用户输入的字符串并发送给服务器
     char bufSend[BUF_SIZE] = {0};
-    printf("Input a string: ");
-    scanf("%s", bufSend);
-    send(sock, bufSend, strlen(bufSend), 0);
-    //接收服务器传回的数据
     char bufRecv[BUF_SIZE] = {0};
-    recv(sock, bufRecv, BUF_SIZE, 0);
 
-    //输出接收到的数据
-    printf("Message form server: %s\n", bufRecv);
+    while (1)
+    {
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        connect(sock, (struct sockaddr *)&sockAddr, sizeof(sockAddr));
+        //获取用户输入的字符串并发送给服务器
 
-    //关闭套接字
-    close(sock);
+        printf("Input a string: ");
+        scanf("%s", bufSend);
+        send(sock, bufSend, strlen(bufSend), 0);
+        //接收服务器传回的数据
+
+        recv(sock, bufRecv, BUF_SIZE, 0);
+
+        //输出接收到的数据
+        printf("Message form server: %s\n", bufRecv);
+
+        memset(bufSend, 0, BUF_SIZE); //重置缓冲区
+        memset(bufRecv, 0, BUF_SIZE); //重置缓冲区
+                                      //关闭套接字
+        close(sock);
+    }
 
     return 0;
 }
